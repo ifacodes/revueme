c6f007388f13822e64b6ccc6b57b795370cc6700
[poww10s] <[theironminer@gmail.com]>
2020-08-28 17:23:43 -0400

    using vs-js-modal for modals now

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <button @click="buttonTrigger('login')">show modal</button>
+    <login-modal ref="login" />
     <HelloWorld msg="Hello World"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import LoginModal from './components/LoginModal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    LoginModal
+  },
+  methods: {
+    buttonTrigger(t_ref) {
+      this.$refs[t_ref].show()
+    }
+  },
+  mounted() {
+    console.log('*mounted', this.$refs);
   }
 }
 </script>
---
+++ front/src/components/LoginModal.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <modal name="modal-login">
+        Login Modal Test
+    </modal>
+    
+</template>
+
+<script>
+export default {
+    name: "login-modal",
+    methods: {
+        show() {
+            this.$modal.show('modal-login');
+        },
+        hide() {
+            this.$modal.hide('modal-login');
+        }
+    },
+    mount() {
+        this.hide()
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
